6a5c553f3a1965b9a18d042b3c8ff806b2046747
Dhaval Maniyar <54331181+dhavalmaniyar@users.noreply.github.com>
2021-02-16 19:39:51 +0530

    Update settings.py

--- solving_scale/settings.py
+++ solving_scale/settings.py
@@ -119,7 +119,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = "dhavalmaniyar123@gmail.com"
-EMAIL_HOST_PASSWORD = 'Dh@v@l777'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = '587'
 
 
